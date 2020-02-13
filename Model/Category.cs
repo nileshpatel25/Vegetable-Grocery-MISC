cb61ad3d826ed02c0274a7eb5641bd2859dd98cc
nileshpatel25 <nileshpatel2533@gmail.com>
2020-02-13 15:36:11 +0530

    Add category model

---
+++ Model/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vegetable_Grocery_MISC.Model
+{
+  public class Category
+  {
+  }
+}
